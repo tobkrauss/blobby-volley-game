8f5b8d3508ae32fc03cc565a81d2594fa0a16a52
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-24 10:45:59 +0100

    Implement score

--- ball.js
+++ ball.js
@@ -1,11 +1,14 @@
+let playerScore = 0;
+let opponentScore = 0;
+
 class Ball {
     constructor() {
         this.width = 80
         this.height = 80
-        this.x = 312,5
-        this.y = 300
+        this.x = 200
+        this.y = Math.random()*200
         this.xspeed = 5
-        this.yspeed = 2
+        this.yspeed = -2
     }
 
     draw() {
@@ -34,15 +37,19 @@ if (this.y >= 600 - this.height || this.y <= 0) {
    this.yspeed = -this.yspeed;
   }
 
- /*  let score = 0
 
     fill("white")
-    textSize(32)
-    text ("Score: " + score, 1050, 50, 300, 300)
-
+    textSize(36)
+    text ("Score " + opponentScore, 1050, 50, 300, 300)
+    text ("Score " + playerScore, 100, 50, 300, 300)
+    
     if (this.y >= 600 - this.height && this.x <= 615) {
-       score++
-    } */
+      opponentScore = opponentScore+1
+    }
+    
+    if (this.y >= 600 - this.height && this.x > 635) {
+      playerScore = playerScore+1
+    }
 
     }
 
